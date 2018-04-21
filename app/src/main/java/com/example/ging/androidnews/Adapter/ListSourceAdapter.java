c76484ca7cb40af58abc59eda981ff84db1a5165
ging7 <g776808@gmail.com>
2018-04-21 12:19:40 +0800

    ItemClickListener

---
+++ app/src/main/java/com/example/ging/androidnews/Adapter/ListSourceAdapter.java
@@ -0,0 +1,71 @@
+package com.example.ging.androidnews.Adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.ging.androidnews.Interface.ItemClickListener;
+import com.example.ging.androidnews.Mode.WebSite;
+import com.example.ging.androidnews.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by alove on 2018/4/21.
+ */
+
+class ListSourceViewHolder extends RecyclerView.ViewHolder
+    implements View.OnClickListener
+{
+
+    ItemClickListener itemClickListener;
+
+    TextView souce_title;
+    CircleImageView source_image;
+
+    public ListSourceViewHolder(View itemView) {
+        super(itemView);
+
+        source_image = (CircleImageView) itemView.findViewById(R.id.soruce_image);
+        souce_title = (TextView) itemView.findViewById(R.id.source_name);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+    }
+}
+
+public class ListSourceAdapter extends RecyclerView.Adapter<ListSourceViewHolder>{
+    private Context context;
+    private WebSite webSite;
+
+    public ListSourceAdapter(Context context, WebSite webSite) {
+        this.context = context;
+        this.webSite = webSite;
+    }
+
+    @Override
+    public ListSourceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.source_layout,parent,false);
+        return new ListSourceViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ListSourceViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return WebSite.getSources().size();
+    }
+}
---
+++ app/src/main/java/com/example/ging/androidnews/Interface/ItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.ging.androidnews.Interface;
+
+import android.view.View;
+
+/**
+ * Created by alove on 2018/4/21.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
